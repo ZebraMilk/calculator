e263c819aacf6bd3985d06bf9c9d62a372f4423f
ZebraMilk <zebramilkx@gmail.com>
2022-02-15 12:33:17 -0600

    Add basic style/spacing framework

--- script.js
+++ script.js
@@ -1,12 +1,71 @@
-const add = (numA, numB) => numA + numB;
 
-const subtract = (numA, numB) => numA - numB;
 
-const multiply = (numA, numB) => numA * numB;
+// Shortcut to assign grid-areas to each button
+let btns = document.querySelectorAll(".btn");
+btns.forEach(btn =>
+  {
+    btn.style.cssText = `grid-area: ${btn.id};`;
+    btn.addEventListener("click", (e) => console.log(e.target.innerText));
+  });
+  
+const operators = [...document.getElementsByClassName("operator")];
+const numbers = [...document.getElementsByClassName("number")];
+const clear = [...document.getElementById("clear")];
+const backspace = [...document.getElementById("backspace")];
+const equals = [...document.getElementById("equals")];
+const decimal = [...document.getElementById("decimal")];
 
-const divide = (numA, numB) => numA / numB;
+let userInput = 0;
+let memory = null;
+let operator = '~';
+let result = 0
 
-const operate = function(operator, numA, numB) {
+let resultDisplay = document.querySelector(".result");
+let inputDisplay = document.querySelector("#input-display");
+let operatorDisplay = document.querySelector(".operation");
+let memoryDisplay = document.querySelector(".memory");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function updateInput() {};
+
+function updateResult() {};
+function updateOperator() {};
+function updateMemory() {};
+
+
+// Restore all values to default
+function clearAll() {
+  userInput = 0;
+  memory = null;
+  operator = '~';
+};
+  
+function add (numA, numB) {return numA + numB};
+
+function subtract (numA, numB) {return numA - numB};
+
+function multiply (numA, numB) {return numA * numB};
+
+function divide (numA, numB) {return numA / numB};
+
+function operate(operator, numA, numB) {
   if(operator == "+") {
     return add(numA, numB);
   } else if (operator == "-") {
@@ -15,13 +74,7 @@ const operate = function(operator, numA, numB) {
     return multiply(numA, numB);
   } else if (operator == "/") {
     return divide(numA, numB);
+  } else{
+    return '~';
   }
 };
-
-
-
-const btns = document.querySelectorAll(".btn");
-btns.forEach(btn =>
-  {
-    btn.style.cssText = `grid-area: ${btn.id};`
-  })
